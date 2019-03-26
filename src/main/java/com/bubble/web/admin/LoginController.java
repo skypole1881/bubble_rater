4b1cf7729cf57c61cb88a0abf2097a954da13881
James <james101511@yahoo.com.tw>
2019-03-26 23:21:04 +0800

    Fix bugs

--- src/main/java/com/bubble/web/admin/LoginController.java
+++ src/main/java/com/bubble/web/admin/LoginController.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,7 +26,7 @@ public class LoginController {
 
 	}
 
-	@GetMapping("/login")
+	@PostMapping("/login")
 	public String login(@RequestParam String username, @RequestParam String password, HttpSession session,
 			RedirectAttributes attributes) {
 		User user = userService.checkUser(username, password);
