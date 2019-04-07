03194f3063011f13b2d0ee6dc20e6046f18f8137
James <james101511@yahoo.com.tw>
2019-04-07 19:16:34 +0800

    [bubble] replace the input name so that the table can be inserted

--- src/main/java/com/bubble/web/BlogController.java
+++ src/main/java/com/bubble/web/BlogController.java
@@ -23,58 +23,58 @@ import com.bubble.service.BlogService;
 public class BlogController {
 
 	private static final String INPUT = "admin/blogs-input";
-    private static final String LIST = "admin/blogs";
-    private static final String REDIRECT_LIST = "redirect:/admin/blogs";
+	private static final String LIST = "admin/blogs";
+	private static final String REDIRECT_LIST = "redirect:/admin/blogs";
 
+	@Autowired
+	private BlogService blogService;
 
-    @Autowired
-    private BlogService blogService;
+	@GetMapping("/blogs")
+	public String blogs(
+			@PageableDefault(size = 8, sort = { "blogId" }, direction = Sort.Direction.ASC) Pageable pageable,
+			Blog blog, Model model) {
+		model.addAttribute("page", blogService.listBlog(pageable, blog));
+		return LIST;
+	}
 
-    @GetMapping("/blogs")
-    public String blogs(@PageableDefault(size = 8, sort = {"blogId"}, direction = Sort.Direction.ASC) Pageable pageable,
-                        Blog blog, Model model) {
-        model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return LIST;
-    }
+	@PostMapping("/blogs/search")
+	public String search(
+			@PageableDefault(size = 8, sort = { "blogId" }, direction = Sort.Direction.ASC) Pageable pageable,
+			Blog blog, Model model) {
+		model.addAttribute("page", blogService.listBlog(pageable, blog));
+		return "admin/blogs :: blogList";
+	}
 
-    @PostMapping("/blogs/search")
-    public String search(@PageableDefault(size = 8, sort = {"blogId"}, direction = Sort.Direction.ASC) Pageable pageable,
-                         Blog blog, Model model) {
-        model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return "admin/blogs :: blogList";
-    }
+	@GetMapping("/blogs/{id}/input")
+	public String editInput(@PathVariable Integer id, Model model) {
+		Blog blog = blogService.getBlog(id);
+		model.addAttribute("blog", blog);
+		return INPUT;
+	}
 
-//    @GetMapping("/blogs/{id}/input")
-//    public String editInput(@PathVariable Integer id, Model model) {
-//        Blog blog = blogService.getBlog(id);
-//        model.addAttribute("blog",blog);
-//        return INPUT;
-//    }
-    @GetMapping("/blogs/input")
-    public String input(Model model) {
-        model.addAttribute("blog", new Blog());
-        return INPUT;
-    }
+	@GetMapping("/blogs/input")
+	public String input(Model model) {
+		model.addAttribute("blog", new Blog());
+		return INPUT;
+	}
 
-
-    @PostMapping("/blogs")
-    public String post(Blog blog, RedirectAttributes attributes, HttpSession session) {
+	@PostMapping("/blogs")
+	public String post(Blog blog, RedirectAttributes attributes, HttpSession session) {
 //        blog.setUser((User) session.getAttribute("user"));
-        Blog b;
-        if (blog.getBlogId() == null) {
-            b =  blogService.saveBlog(blog);
-        } else {
-            b = blogService.updateBlog(blog.getBlogId(), blog);
-        }
+		Blog b;
+		if (blog.getBlogId() == null) {
+			b = blogService.saveBlog(blog);
+		} else {
+			b = blogService.updateBlog(blog.getBlogId(), blog);
+		}
 
-        if (b == null ) {
-            attributes.addFlashAttribute("message", "操作失败");
-        } else {
-            attributes.addFlashAttribute("message", "操作成功");
-        }
-        return REDIRECT_LIST;
-    }
-
+		if (b == null) {
+			attributes.addFlashAttribute("message", "操作失败");
+		} else {
+			attributes.addFlashAttribute("message", "操作成功");
+		}
+		return REDIRECT_LIST;
+	}
 
 //    @GetMapping("/blogs/{id}/delete")
 //    public String delete(@PathVariable Long id,RedirectAttributes attributes) {
