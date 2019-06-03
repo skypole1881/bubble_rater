c1d07e330bc600bf7fa06fa3f736f58872345593
James <yuchen.chih@logicsolutions.com>
2019-06-03 20:51:50 +0800

    [Add] add get API for single page of the store

--- src/main/java/com/bubble/web/IndexController.java
+++ src/main/java/com/bubble/web/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,6 +41,13 @@ public class IndexController {
 		model.addAttribute("blogs", dtos);
 		model.addAttribute("default", defaultList);
 		return "home";
+	}
+	
+	@GetMapping("/{Id}")
+	public String indexSingle(Model model,@PathVariable("Id") Integer Id) {
+		Blog blog = blogService.getBlog(Id);
+		model.addAttribute("blog", blog);
+		return "single-store";
 	}
 
 	//這裡看要不要改成POST
