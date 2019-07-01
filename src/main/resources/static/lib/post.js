a7966c47c35229cf674b36a90fe963e603383c02
indih <indih1232@gmail.com>
2019-07-02 00:44:22 +0800

    [Modify] let search not only search from beginning and user can deselect option

--- src/main/resources/static/lib/post.js
+++ src/main/resources/static/lib/post.js
@@ -1,4 +1,7 @@
 //initial chosen-select dropdown
-$( ".chosen-select" ).chosen();
+$( ".chosen-select" ).chosen({
+	search_contains: true,
+	allow_single_deselect: true
+});
 //let lazyload class img lazyload
 //lazyload();
