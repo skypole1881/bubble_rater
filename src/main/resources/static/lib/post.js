9dd7bdf9ac5fcab68f7a53640a2e2d510f1a177f
indih <indih1232@gmail.com>
2019-07-14 02:38:00 +0800

    [Modify] let top 3 store can touch in mobile replace hover

--- src/main/resources/static/lib/post.js
+++ src/main/resources/static/lib/post.js
@@ -5,3 +5,27 @@ $( ".chosen-select" ).chosen({
 });
 //let lazyload class img lazyload
 lazyload();
+
+$(".change-top").on("touchend", touchEnd);
+function touchEnd(event){
+	if( $(this).hasClass("store")){
+		if( $(this).hasClass("bg-black")){
+			$(this).removeClass("bg-black");
+			$(this).children(".change-second").css("opacity", 0);
+			$(this).children(".change-first").removeClass("d-none");
+			$(this).children(".change-first").addClass("d-block");
+		}
+		else{
+			$(".store.bg-black .change-first").removeClass("d-none");
+			$(".store.bg-black .change-first").addClass("d-block");
+			$(".store.bg-black .change-second").css("opacity", 0);
+			$(".store.bg-black").removeClass("bg-black");
+			$(this).addClass("bg-black");
+			$(this).children(".change-first").removeClass("d-block");
+			$(this).children(".change-first").addClass("d-none");
+			$(this).children(".change-second").css("opacity", 1);
+		}
+	}
+	else{
+	}
+}
