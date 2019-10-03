0137c26f3ba48de43418bf2686c35ba0b13cb0c7
lohsts <lohsts@gmail.com>
2019-10-03 23:26:31 +0800

    Add BRBaseViewController.

---
+++ iOS/bubblerater/Controller/Base/BRBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BRBaseViewController.swift
+//  bubblerater
+//
+//  Created by lohsts on 2019/10/3.
+//  Copyright © 2019 lohsts. All rights reserved.
+//
+
+import UIKit
+
+class BRBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
