c6d9be1f27a270eec68cd614c9c2b965c155b513
indih <indih1232@gmail.com>
2019-10-31 23:30:26 +0800

    [Modify] modify list and post functions to two correspond components

--- backend-react/src/App.js
+++ backend-react/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 // import 'https://cdn.jsdelivr.net/semantic-ui/2.2.4/semantic.min.css'
 import 'semantic-ui/dist/semantic.min.css';
 import './me.css';
+import List from './component/List'
+import Post from './component/Post'
 
 function Header() {
   return (
@@ -12,14 +14,14 @@ function Header() {
       <nav className="ui inverted attached segment m-padded-tb-mini m-shadow-small" >
         <div className="ui container">
           <div className="ui inverted secondary stackable menu">
-            <h2 className="ui teal header item">管理后台</h2>
+            <h2 className="ui teal header item">管理後台</h2>
             <a href="/admin/blogs.html" className=" m-item item m-mobile-hide active"><i className="mini home icon"></i>博客</a>
             <div className="right m-item m-mobile-hide menu">
               <div className="ui dropdown  item">
                 <div className="text"></div>
                 <i className="dropdown icon"></i>
                 <div className="menu">
-                  <a href="./logout" className="item">注销</a>
+                  <a href="./logout" className="item">註銷</a>
                 </div>
               </div>
             </div>
@@ -36,46 +38,13 @@ function Header() {
 class MainContent extends React.Component {
   constructor(props){
     super(props);
-        this.state = {
-            numberofpage: 1,
-            stores: [],
-        }
-  }
-  componentDidMount() {
-    fetch('http://13.230.125.32:8080/v1/bubble/detail/list')
-        .then(res => res.json())
-        .then((data) => {
-            this.setState({ stores: data })
-            console.log(this.state.stores[0])
-        })
-        .catch()
   }
   render () {
     return (
-      <table className="ui compact teal table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>品牌</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.state.stores.map(
-              (store, index) => (
-                <tr>
-                  <td>{index}</td>
-                  <td>{store.id}</td>
-                  <td>
-                    <a href={"/admin/blogs/"+store.id+"/input(id="+store.id+")"} className="ui mini teal basic button">编辑</a>
-                    <a href={"/admin/blogs/"+store.id+"/delete(id="+store.id+")"} className="ui mini red basic button">删除</a>
-                  </td>
-                </tr>
-              )
-            )
-          }
-        </tbody>
-      </table>
+      <div>
+        <List/>
+        <Post id = "1" />
+      </div>
     )
   }
 }
@@ -92,7 +61,7 @@ function App() {
         </div>
       </div>
 
-      <div  className="m-container-small m-padded-tb-big">
+      <div className="m-container-small m-padded-tb-big">
         <div className="ui container">
           <div  className="ui secondary segment form">
             <input type="hidden" name="page" />
