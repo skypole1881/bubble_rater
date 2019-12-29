32400387da28a01f716a096af4c6bcd0b805c608
Howard Yuan <howard.yuan@logicsolutions.com>
2019-12-29 21:13:18 +0800

    refactor: remove BrowserRouter user Router

--- bubble-react/src/App.js
+++ bubble-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleRoot} from 'radium';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
 import './single-store.css'
 import './blog-home.css'
--- bubble-react/src/index.js
+++ bubble-react/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route>
+  <Router>
         <App />
-      </Route>
-    </Switch>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
